b9f68c8a8a96370bd38398ab2bf416ba4df44581
Nastia <the_sun.89@mail.ru>
2026-02-01 22:26:00 +0300

    feat: add new app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+// import { Header } from './Header'
+function App() {
+  return (
+
+   <div>App</div>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import logo from './assets/img/logo.svg'
+
+export const Header = () => {
+  return (
+    <div className="header">
+	<img src={logo} alt="logo" />
+    </div>
+  )
+}
